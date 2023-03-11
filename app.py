623bfd7e591b3a9c55469e1f660161eb051980df
nishanth <nishanthsentyar@gmail.com>
2023-03-11 15:33:20 +0530

    tutorial 5

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
+from datetime import timedelta
 
 
 app = Flask(__name__)
-
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes = 5)
 
 @app.route('/')
 def home():
@@ -11,14 +13,27 @@ def home():
 @app.route('/login', methods = ['POST','GET'])
 def login():
     if request.method == "POST":
-        user = request.form["name"]
-        return redirect(url_for("user",usr = user))
-    else:    
+        session.permanent = True
+        user = request.form["name"]  #<input type="text" name="namer"
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
         return render_template("login.html")
 
-@app.route('/<usr>')
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route('/user')
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
+    
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
