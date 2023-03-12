253b554dbc65c6fe6b32ef3b42a3b37e91d9eda9
nishanth <nishanthsentyar@gmail.com>
2023-03-12 14:40:46 +0530

    Backup 36minutes

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.secret_key = "Secret Key"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:'adminroot@123'@localhost/crud"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  #Doesnt display warnings
+
+db = SQLAlchemy(app)
+
+
 
 
 @app.route('/')
